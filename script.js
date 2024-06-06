30ad4bb9631345fef1896eefea896d6bba843412
Dart-Nijanthan <nijanthan@dartinnovations.net>
2024-06-06 18:33:46 +0530

    added works section

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 'use strict';
 
-var swiper = new Swiper('.mySwiper', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+const hiddenWorks = document.querySelectorAll('.hidden');
+const showWorkBtn = document.querySelector('.show_btn');
+
+showWorkBtn.addEventListener('click', function () {
+  hiddenWorks.forEach((el) => {
+    el.classList.toggle('hide');
+  });
+  if (hiddenWorks[0].classList.contains('hide')) {
+    showWorkBtn.textContent = 'show more';
+  } else {
+    showWorkBtn.textContent = 'show less';
+  }
 });
