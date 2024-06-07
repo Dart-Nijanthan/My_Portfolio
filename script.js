bd25935cad02b84f04d4b5db2e1d0c19909900f4
Dart-Nijanthan <nijanthan@dartinnovations.net>
2024-06-07 16:10:38 +0530

    added contacts and intersection observer

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 const hiddenWorks = document.querySelectorAll('.hidden');
 const showWorkBtn = document.querySelector('.show_btn');
+const sectionHero = document.querySelector('.section_hero');
+const nav = document.querySelector('.nav');
+const sections = document.querySelectorAll('.section');
 
 showWorkBtn.addEventListener('click', function () {
   hiddenWorks.forEach((el) => {
@@ -12,4 +15,37 @@ showWorkBtn.addEventListener('click', function () {
   } else {
     showWorkBtn.textContent = 'show less';
   }
+});
+
+const stickyOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px',
+};
+
+const stickyOberver = new IntersectionObserver(function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('nav_sticky');
+  else nav.classList.remove('nav_sticky');
+}, stickyOptions);
+
+stickyOberver.observe(sectionHero);
+
+const revealOptions = {
+  root: null,
+  threshold: 0.15,
+};
+
+const revealObserver = new IntersectionObserver(function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section_hidden');
+  revealObserver.unobserve(entry.target);
+}, revealOptions);
+
+sections.forEach((section) => {
+  revealObserver.observe(section);
+  section.classList.add('section_hidden');
 });
